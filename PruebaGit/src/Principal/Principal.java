a47ac1de236c12264ed602406b79c6dcb580d159
Ander González <ander.gonzalez.a@opendeusto.es>
2024-09-23 08:20:44 +0200

    pp

--- PruebaGit/src/Principal/Principal.java
+++ PruebaGit/src/Principal/Principal.java
@@ -2,6 +2,7 @@ package Principal;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Estamos a Lunes");
+		System.out.println("Aprendiendo");
+		System.out.println("Buenas");
 	}
 }
